511315d6d8badb7d25ded2645a59d066d1930053
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-20 20:59:29 +0700

    add string to lower function

--- src/String/string_native.c
+++ src/String/string_native.c
@@ -32,6 +32,27 @@ Value native_string_uppercase(int arg_count, Value* args) {
     return res_val;
 }
 
+Value native_string_lowercase(int arg_count, Value* args) {
+    if (arg_count < 1 || args[0].type != VAL_STRING) {
+        return (Value){VAL_NIL, {0}};
+    }
+
+    const char* source = args[0].as.string;
+    int length = strlen(source);
+    
+    char* result = malloc(length + 1);
+    if (result == NULL) return (Value){VAL_NIL, {0}};
+    
+    for (int i = 0; i < length; i++) {
+        result[i] = (char)tolower((unsigned char)source[i]);
+    }
+    result[length] = '\0';
+
+    Value res_val = (Value){VAL_STRING, {.string = result}};
+    return res_val;
+}
+
 void register_string_natives(Env* env) {
-    STRING_REGISTER(env, "to_uppercase", native_string_uppercase);
+    STRING_REGISTER(env, "__str_toUpper", native_string_uppercase);
+    STRING_REGISTER(env, "__str_toLower", native_string_lowercase);
 }
