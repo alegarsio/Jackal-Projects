83cef2a35701ed24a139203c9e629fcb51867024
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-20 20:45:07 +0700

    add string native implementation in c

---
+++ src/String/string_native.c
@@ -0,0 +1,37 @@
+#include "String/string_native.h"
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define STRING_REGISTER(env, name, func) \
+    do { \
+        if (func != NULL) { \
+            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
+        } else { \
+            printf("Socket Error: Native function '%s' not implemented!\n", name); \
+        } \
+    } while (0)
+
+
+Value native_string_uppercase(int arg_count, Value* args) {
+    if (arg_count < 1 || args[0].type != VAL_STRING) {
+        return (Value){VAL_NIL, {0}};
+    }
+
+    const char* source = args[0].as.string;
+    int length = strlen(source);
+    
+    char* result = malloc(length + 1);
+    
+    for (int i = 0; i < length; i++) {
+        result[i] = toupper((unsigned char)source[i]);
+    }
+    result[length] = '\0';
+
+    Value res_val = (Value){VAL_STRING, {.string = result}};
+    return res_val;
+}
+
+void register_string_natives(Env* env) {
+    STRING_REGISTER(env, "to_uppercase", native_string_uppercase);
+}
