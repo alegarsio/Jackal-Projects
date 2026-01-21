a25ed2e72cca4262b04352fd79b5e4a144e79931
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-21 18:07:02 +0700

    add sys platform

--- src/System/system_native.c
+++ src/System/system_native.c
@@ -12,6 +12,25 @@
         } \
     } while (0)
 
+Value native_system_platform(int arg_count, Value* args) {
+    #ifdef _WIN32
+        return (Value){VAL_STRING, {.string = strdup("windows")}};
+    #elif __APPLE__
+        #include <TargetConditionals.h>
+        #if TARGET_OS_IPHONE
+            return (Value){VAL_STRING, {.string = strdup("ios")}};
+        #else
+            return (Value){VAL_STRING, {.string = strdup("macos")}};
+        #endif
+    #elif __linux__
+        return (Value){VAL_STRING, {.string = strdup("linux")}};
+    #elif __unix__
+        return (Value){VAL_STRING, {.string = strdup("unix")}};
+    #else
+        return (Value){VAL_STRING, {.string = strdup("unknown")}};
+    #endif
+}
+
 Value native_system_exec(int arg_count, Value* args) {
     if (arg_count < 1 || args[0].type != VAL_STRING) {
         return (Value){VAL_NUMBER, {.number = -1}};
@@ -22,8 +41,7 @@ Value native_system_exec(int arg_count, Value* args) {
 }
 
 
-
-
-void register_string_natives(Env* env){
+void register_sys_natives(Env* env){
     SYS_REGISTER(env,"__sys_run",native_system_exec);
+    SYS_REGISTER(env,"__sys_platform",native_system_platform);
 }
