b955fd06bc4ca5d718e890e2533252199f322f52
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-21 17:49:25 +0700

    add the first implementation for native system api

---
+++ src/System/system_native.c
@@ -0,0 +1,29 @@
+#include"System/system_native.h"
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define SYS_REGISTER(env, name, func) \
+    do { \
+        if (func != NULL) { \
+            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
+        } else { \
+            printf("Error: Native function '%s' not implemented!\n", name); \
+        } \
+    } while (0)
+
+Value native_system_exec(int arg_count, Value* args) {
+    if (arg_count < 1 || args[0].type != VAL_STRING) {
+        return (Value){VAL_NUMBER, {.number = -1}};
+    }
+    
+    int result = system(args[0].as.string);
+    return (Value){VAL_NUMBER, {.number = (double)result}};
+}
+
+
+
+
+void register_string_natives(Env* env){
+    SYS_REGISTER(env,"__sys_run",native_system_exec);
+}
