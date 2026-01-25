432b4dd2deafcec963431effbdefc2625a6b296f
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-25 17:47:46 +0700

    add first math class function math abs and register to env

---
+++ src/math/native_math.c
@@ -0,0 +1,22 @@
+#include"math/math_native.h"
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+
+#define MATH_REGISTER(env, name, func) \
+    do { \
+        if (func != NULL) { \
+            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
+        } else { \
+            printf("Error: Native function '%s' not implemented!\n", name); \
+        } \
+    } while (0)
+
+Value native_math_abs(int arg_count, Value* args) {
+    if (arg_count < 1 || args[0].type != VAL_NUMBER) return (Value){VAL_NUMBER, {.number = 0}};
+    return (Value){VAL_NUMBER, {.number = fabs(args[0].as.number)}};
+}
+
+void register_math_natives(Env* env){
+    MATH_REGISTER(env,"__math_abs",native_math_abs);
+}
