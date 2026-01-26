b02be4bdfad46c432100a7965f5ea2187c8d3f97
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-26 19:13:53 +0700

    add string inputfunction

--- src/Io/io_native.c
+++ src/Io/io_native.c
@@ -1,31 +1,42 @@
-#include"Io/io_native.h"
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
+#include "Io/io_native.h"
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-
-#define IO_REGISTER(env, name, func) \
-    do { \
-        if (func != NULL) { \
+#define IO_REGISTER(env, name, func)                                             \
+    do                                                                           \
+    {                                                                            \
+        if (func != NULL)                                                        \
+        {                                                                        \
             set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
-        } else { \
-            printf("Error: Native function '%s' not implemented!\n", name); \
-        } \
+        }                                                                        \
+        else                                                                     \
+        {                                                                        \
+            printf("Error: Native function '%s' not implemented!\n", name);      \
+        }                                                                        \
     } while (0)
 
-
-    char* get_input_raw(const char* prompt) {
-    if (prompt) {
+char *get_input_raw(const char *prompt)
+{
+    if (prompt)
+    {
         printf("%s", prompt);
         fflush(stdout);
     }
     static char buffer[1024];
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL) return NULL;
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+        return NULL;
     buffer[strcspn(buffer, "\n")] = 0;
     return buffer;
 }
 
+Value native_input_string(int arity, Value *args) {
+    char* input = get_input_raw(arity > 0 ? args[0].as.string : NULL);
+    if (!input) return (Value){VAL_NIL, {0}};
+    return (Value){VAL_STRING, {.string = strdup(input)}};
+}
 
-void register_io_natives(Env* env){
-    
+
+void register_io_natives(Env *env)
+{
 }
