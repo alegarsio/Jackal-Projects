1c796411af128255a52eaf762fe5f03433c7067d
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-26 20:01:15 +0700

    add __io_autostream to io register

--- src/Io/io_native.c
+++ src/Io/io_native.c
@@ -41,8 +41,48 @@ Value native_input_number(int arity, Value *args) {
     if (!input) return (Value){VAL_NUMBER, {.number = 0}};
     return (Value){VAL_NUMBER, {.number = atof(input)}};
 }
+
+
+Value native_reads(int arity, Value *args) {
+
+    if (arity > 0 && args[0].type == VAL_STRING) {
+        printf("%s", args[0].as.string);
+        fflush(stdout);
+    }
+
+    char buffer[1024];
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        return (Value){VAL_NIL, .as = {0}};
+    }
+
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
+
+    char *endptr;
+    double num = strtod(buffer, &endptr);
+
+    if (strlen(buffer) == 0) {
+        char *empty = malloc(1);
+        empty[0] = '\0';
+        return (Value){VAL_STRING, {.string = empty}};
+    }
+
+    if (*endptr == '\0') {
+        return (Value){VAL_NUMBER, {.number = num}};
+    }
+
+    char *str_copy = malloc(strlen(buffer) + 1);
+    if (str_copy == NULL) return (Value){VAL_NIL, .as = {0}};
+    strcpy(str_copy, buffer);
+
+    return (Value){VAL_STRING, {.string = str_copy}};
+}
+
 void register_io_natives(Env *env)
 {
     IO_REGISTER(env,"__io_string",native_input_string);
     IO_REGISTER(env,"__io_number",native_input_number);
+    IO_REGISTER(env,"__io_autostream",native_reads);
 }
