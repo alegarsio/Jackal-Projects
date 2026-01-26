293564760f3043bccb48f4bcd3eb4bc094d65cb8
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-26 19:12:45 +0700

    add base function for Io

---
+++ src/Io/io_native.c
@@ -0,0 +1,31 @@
+#include"Io/io_native.h"
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+
+
+#define IO_REGISTER(env, name, func) \
+    do { \
+        if (func != NULL) { \
+            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
+        } else { \
+            printf("Error: Native function '%s' not implemented!\n", name); \
+        } \
+    } while (0)
+
+
+    char* get_input_raw(const char* prompt) {
+    if (prompt) {
+        printf("%s", prompt);
+        fflush(stdout);
+    }
+    static char buffer[1024];
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) return NULL;
+    buffer[strcspn(buffer, "\n")] = 0;
+    return buffer;
+}
+
+
+void register_io_natives(Env* env){
+    
+}
