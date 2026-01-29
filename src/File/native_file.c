9d2bfbfffe8ff166ac6f8a604334fbaef204ef15
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-29 08:14:59 +0700

    add new native file exist function

--- src/File/native_file.c
+++ src/File/native_file.c
@@ -1,36 +1,57 @@
-#include"File/native_file.h"
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-#include<math.h>
+#include "File/native_file.h"
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
 
-#define FILE_REGISTER(env, name, func) \
-    do { \
-        if (func != NULL) { \
+#define FILE_REGISTER(env, name, func)                                           \
+    do                                                                           \
+    {                                                                            \
+        if (func != NULL)                                                        \
+        {                                                                        \
             set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
-        } else { \
-            printf("Error: Native function '%s' not implemented!\n", name); \
-        } \
+        }                                                                        \
+        else                                                                     \
+        {                                                                        \
+            printf("Error: Native function '%s' not implemented!\n", name);      \
+        }                                                                        \
     } while (0)
 
-    Value native_file_read(int arity, Value *args) {
-        if (arity < 1 || args[0].type != VAL_STRING) return (Value){VAL_NIL, {0}};
+Value native_file_read(int arity, Value *args)
+{
+    if (arity < 1 || args[0].type != VAL_STRING)
+        return (Value){VAL_NIL, {0}};
 
-        FILE *file = fopen(args[0].as.string, "r");
-        if (!file) return (Value){VAL_NIL, {0}};
+    FILE *file = fopen(args[0].as.string, "r");
+    if (!file)
+        return (Value){VAL_NIL, {0}};
 
-        fseek(file, 0, SEEK_END);
-        long fsize = ftell(file);
-        fseek(file, 0, SEEK_SET);
+    fseek(file, 0, SEEK_END);
+    long fsize = ftell(file);
+    fseek(file, 0, SEEK_SET);
 
-        char *string = malloc(fsize + 1);
-        fread(string, fsize, 1, file);
+    char *string = malloc(fsize + 1);
+    fread(string, fsize, 1, file);
+    fclose(file);
+
+    string[fsize] = 0;
+    return (Value){VAL_STRING, {.string = string}};
+}
+Value native_file_exists(int arity, Value *args)
+{
+    if (arity < 1 || args[0].type != VAL_STRING)
+        return (Value){VAL_NUMBER, {.number = 0}};
+
+    FILE *file = fopen(args[0].as.string, "r");
+    if (file)
+    {
         fclose(file);
-
-        string[fsize] = 0;
-        return (Value){VAL_STRING, {.string = string}};
+        return (Value){VAL_NUMBER, {.number = 1}};
     }
+    return (Value){VAL_NUMBER, {.number = 0}};
+}
 
-void register_file_natives(Env* env){
-    FILE_REGISTER(env,"__ioFile_read",native_file_read);
+void register_file_natives(Env *env)
+{
+    FILE_REGISTER(env, "__ioFile_read", native_file_read);
 }
