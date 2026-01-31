12b297a0b950f01a84675617044e4c53f6a8296c
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-31 09:11:14 +0700

    add native http and rename the function and struct

--- src/http/native_http.c
+++ src/http/native_http.c
@@ -20,61 +20,99 @@
     } while (0)
 
 
-struct MemoryStruct {
-  char *memory;
-  size_t size;
+struct HttpBuffer {
+    char *data;
+    size_t size;
 };
 
-static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+    struct HttpBuffer *mem = (struct HttpBuffer *)userp;
 
-  char *ptr = realloc(mem->memory, mem->size + realsize + 1);
-  if(!ptr) return 0; 
+    char *ptr = realloc(mem->data, mem->size + realsize + 1);
+    if (!ptr) return 0; // Out of memory
 
-  mem->memory = ptr;
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
+    mem->data = ptr;
+    memcpy(&(mem->data[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->data[mem->size] = 0;
 
-  return realsize;
+    return realsize;
 }
-
 Value native_http_get(int arity, Value *args) {
-    if (arity < 1 || args[0].type != VAL_STRING) return (Value){VAL_NIL, {0}};
+    
+    if (arity < 1 || args[0].type != VAL_STRING) {
+        return (Value){VAL_NIL, {0}};
+    }
 
     CURL *curl_handle;
     CURLcode res;
-    struct MemoryStruct chunk;
-
-    chunk.memory = malloc(1); 
-    chunk.size = 0; 
+    struct HttpBuffer chunk = {malloc(1), 0};
 
     curl_global_init(CURL_GLOBAL_ALL);
     curl_handle = curl_easy_init();
 
-    curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-    res = curl_easy_perform(curl_handle);
-
-    Value result;
-    if(res != CURLE_OK) {
-        result = (Value){VAL_NIL, {0}};
-    } else {
+    if (curl_handle) {
+        curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
         
-        result = (Value){VAL_STRING, {.string = strdup(chunk.memory)}};
+        curl_easy_setopt(curl_handle, CURLOPT_TIMEOUT, 10L);
+
+        res = curl_easy_perform(curl_handle);
+
+        if (res != CURLE_OK) {
+            free(chunk.data);
+            curl_easy_cleanup(curl_handle);
+            return (Value){VAL_NIL, {0}};
+        }
+
+        curl_easy_cleanup(curl_handle);
     }
 
-    curl_easy_cleanup(curl_handle);
-    free(chunk.memory);
-    curl_global_cleanup();
-
+    Value result = (Value){VAL_STRING, {.string = strdup(chunk.data)}};
+    free(chunk.data);
     return result;
 }
 
+Value native_http_post(int arity, Value *args) {
+    if (arity < 2 || args[0].type != VAL_STRING || args[1].type != VAL_STRING) {
+        return (Value){VAL_NIL, {0}};
+    }
+
+    CURL *curl_handle;
+    CURLcode res;
+    struct HttpBuffer chunk = {malloc(1), 0};
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+
+    if (curl_handle) {
+        curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
+        
+        curl_easy_setopt(curl_handle, CURLOPT_POST, 1L);
+        
+        curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, args[1].as.string);
+
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Jackal-Interpreter/1.0");
+
+        res = curl_easy_perform(curl_handle);
+
+        if (res != CURLE_OK) {
+            free(chunk.data);
+            curl_easy_cleanup(curl_handle);
+            return (Value){VAL_NIL, {0}};
+        }
+
+        curl_easy_cleanup(curl_handle);
+    }
+
+    Value result = (Value){VAL_STRING, {.string = strdup(chunk.data)}};
+    free(chunk.data);
+    return result;
+}
 void register_http_natives(Env *env){
     HTTP_REGISTER(env,"__http_get",native_http_get);
 }
