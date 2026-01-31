b449726bce5560ab2175bb875919c5068e7f96e8
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-31 12:01:36 +0700

    renew the getstatus function

--- src/http/native_http.c
+++ src/http/native_http.c
@@ -160,31 +160,23 @@ Value native_http_get_headers(int arity, Value *args) {
 }
 
 Value native_http_get_status(int arity, Value *args) {
-    if (arity < 1 || args[0].type != VAL_STRING) {
-        return (Value){VAL_NUMBER, {.number = 0}};
-    }
+    if (arity < 1 || args[0].type != VAL_STRING) return (Value){VAL_NUMBER, {.number = 0}};
 
-    CURL *curl_handle;
-    CURLcode res;
+    CURL *curl_handle = curl_easy_init();
     long response_code = 0;
-
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl_handle = curl_easy_init();
 
     if (curl_handle) {
         curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
-        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Jackal-Interpreter/1.0");
         
-        res = curl_easy_perform(curl_handle);
+        curl_easy_setopt(curl_handle, CURLOPT_NOBODY, 1L); 
+
+        CURLcode res = curl_easy_perform(curl_handle);
 
         if (res == CURLE_OK) {
             curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &response_code);
         }
-
         curl_easy_cleanup(curl_handle);
     }
-
-    curl_global_cleanup();
 
     return (Value){VAL_NUMBER, {.number = (double)response_code}};
 }
@@ -192,4 +184,5 @@ void register_http_natives(Env *env){
     HTTP_REGISTER(env,"__http_get",native_http_get);
     HTTP_REGISTER(env,"__http_post",native_http_post);
     HTTP_REGISTER(env,"__http_header",native_http_get_headers);
+    HTTP_REGISTER(env,"__http_status",native_http_get_status);
 }
