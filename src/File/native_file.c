a3ed32276d1e4dcd88d755b09ae4c4150805cc3a
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-01-31 08:09:21 +0700

    register new file native load function

--- src/File/native_file.c
+++ src/File/native_file.c
@@ -171,4 +171,5 @@ void register_file_natives(Env *env)
     FILE_REGISTER(env,"__ioFile_create",native_file_create);
     FILE_REGISTER(env,"__ioFile_remove",native_file_remove);
     FILE_REGISTER(env,"__ioFile_rename",native_file_rename);
+    FILE_REGISTER(env,"__ioFile_load",native_file_load);
 }
