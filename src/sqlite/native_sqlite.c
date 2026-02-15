248b63604f2488b65f857c7422b42dcbab44be95
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-02-15 17:11:49 +0700

    add new sqlite native function native_db_open

--- src/sqlite/native_sqlite.c
+++ src/sqlite/native_sqlite.c
@@ -1,12 +1,12 @@
-#include"sqlite/native_sqlite.h"
-#include<sqlite3.h>
+#include "sqlite/native_sqlite.h"
+#include <sqlite3.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
 #include "env.h"
 
-#define SQLITE_REGISTER(env, name, func)                                           \
+#define SQLITE_REGISTER(env, name, func)                                         \
     do                                                                           \
     {                                                                            \
         if (func != NULL)                                                        \
@@ -19,7 +19,16 @@
         }                                                                        \
     } while (0)
 
-    
-void register_sqlite_native(Env *env){
+Value native_db_open(int arity, Value *args)
+{
+    sqlite3 *db;
+    if (sqlite3_open(args[0].as.string, &db) != SQLITE_OK)
+    {
+        return (Value){VAL_NIL, {0}};
+    }
+    return (Value){VAL_FILE, {.file = (FILE *)db}};
+}
 
+void register_sqlite_native(Env *env)
+{
 }
