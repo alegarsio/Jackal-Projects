7e974ae9ff9de4aebdb3c0f1c96b10b7d9e1b020
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-02-26 12:50:16 +0700

    add new native key function

--- src/map/native_map.c
+++ src/map/native_map.c
@@ -40,6 +40,34 @@ Value native_map_set_manual(int arity, Value* args) {
     return val; 
 }
 
+Value native_map_keys(int arity, Value* args) {
+    if (arity < 1 || args[0].type != VAL_MAP) {
+        ValueArray* empty_va = array_new();
+        return (Value){VAL_ARRAY, {.array = empty_va}};
+    }
+
+    HashMap* map = args[0].as.map;
+    ValueArray* keys_va = array_new();
+
+    if (map->entries == NULL) {
+        return (Value){VAL_ARRAY, {.array = keys_va}};
+    }
+
+    for (int i = 0; i < map->capacity; i++) {
+        Entry* entry = &map->entries[i];
+        
+        if (entry->key != NULL) {
+            Value key_val;
+            key_val.type = VAL_STRING;
+            key_val.as.string = strdup(entry->key);
+            
+            array_append(keys_va, key_val);
+        }
+    }
+
+    return (Value){VAL_ARRAY, {.array = keys_va}};
+}
+
 void register_map_natives(Env *env){
         MAP_REGISTER(env, "__map_set_manual__", native_map_set_manual);
         MAP_REGISTER(env, "__map_init_manual__", native_map_init_manual);
