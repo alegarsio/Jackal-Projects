7f33133e25e1cc8c9d973031cc01df545faa1521
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-02-27 14:02:34 +0700

    modify the template for mysql not found protection

--- src/mysql/native_mysql.c
+++ src/mysql/native_mysql.c
@@ -1,28 +1,22 @@
-#include"mysql/native_mysql.h"
-
-
+#include "mysql/native_mysql.h"
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <stdio.h>
 #include "env.h"
 
 #define MSQL_REGISTER(env, name, func)                                           \
     do                                                                           \
     {                                                                            \
-        if (func != NULL)                                                        \
-        {                                                                        \
-            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
-        }                                                                        \
-        else                                                                     \
-        {                                                                        \
-            printf("Error: Native function '%s' not implemented!\n", name);      \
-        }                                                                        \
+        set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, "");     \
     } while (0)
 
 #ifdef USE_MYSQL
 #include <mysql/mysql.h>
 
 Value native_mysql_connect(int arity, Value* args) {
+    if (arity < 4) return (Value){VAL_NIL};
+
     MYSQL *conn = mysql_init(NULL);
     if (!mysql_real_connect(conn, args[0].as.string, args[1].as.string, 
                             args[2].as.string, args[3].as.string, 0, NULL, 0)) {
@@ -30,7 +24,16 @@ Value native_mysql_connect(int arity, Value* args) {
     }
     return (Value){VAL_NUMBER, {.number = (uintptr_t)conn}};
 }
-#endif
-void register_mysql_natives(Env *env){
 
+#else
+
+Value native_mysql_connect(int arity, Value* args) {
+    printf("Jackal Error: MySQL support is not enabled in this binary.\n");
+    return (Value){VAL_NIL};
+}
+
+#endif
+
+void register_mysql_natives(Env *env) {
+    MSQL_REGISTER(env, "__mysql_connect__", native_mysql_connect);
 }
