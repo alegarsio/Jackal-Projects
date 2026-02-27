f8e40e6b74636456ab66bc169ebcabf2a0161d62
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-02-27 20:51:16 +0700

    add new native mysql find all function

--- src/mysql/native_mysql.c
+++ src/mysql/native_mysql.c
@@ -180,8 +180,52 @@ Value native_mysql_insert(int arity, Value* args) {
     return (Value){VAL_BOOL, {.boolean = 1}};
 }
 
+Value native_mysql_find_all(int arity, Value* args) {
+    if (arity < 2 || args[0].type != VAL_NUMBER || args[1].type != VAL_STRING) {
+        return (Value){VAL_NIL};
+    }
+
+    MYSQL *conn = (MYSQL*)(uintptr_t)args[0].as.number;
+    const char* table_name = args[1].as.string;
+
+    char sql[256];
+    snprintf(sql, sizeof(sql), "SELECT * FROM %s", table_name);
+
+    if (mysql_query(conn, sql)) {
+        fprintf(stderr, "MySQL FindAll Error: %s\n", mysql_error(conn));
+        return (Value){VAL_NIL};
+    }
+
+    MYSQL_RES *result = mysql_store_result(conn);
+    if (result == NULL) return (Value){VAL_NIL};
+
+    int num_fields = mysql_num_fields(result);
+    MYSQL_FIELD *fields = mysql_fetch_fields(result);
+    MYSQL_ROW row;
+
+    ValueArray* va = value_array_new();
+
+    while ((row = mysql_fetch_row(result))) {
+        HashMap* map = map_new();
+        for (int i = 0; i < num_fields; i++) {
+            Value val;
+            if (row[i]) {
+                val = (Value){VAL_STRING, {.string = strdup(row[i])}};
+            } else {
+                val = (Value){VAL_NIL};
+            }
+            map_set(map, fields[i].name, val);
+        }
+        value_array_push(va, (Value){VAL_MAP, {.map = map}});
+    }
+
+    mysql_free_result(result);
+    return (Value){VAL_ARRAY, {.array = va}};
+}
+
 void register_mysql_natives(Env *env) {
     MSQL_REGISTER(env, "__mysql_connect__", native_mysql_connect);
     MSQL_REGISTER(env, "__mysql_query__", native_mysql_query);
     MSQL_REGISTER(env,"__mysql_create__",native_mysql_create_table);
+    MSQL_REGISTER(env,"__mysql_insert__",native_mysql_insert);
 }
