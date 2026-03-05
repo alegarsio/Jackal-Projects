8f6322b19ff873c9f58acda496440c8fed7219fb
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-03-05 10:10:54 +0700

    register array distinct method

--- src/array/native_array.c
+++ src/array/native_array.c
@@ -20,6 +20,36 @@
         }                                                                        \
     } while (0)
 
-void register_array_natives(Env *env){
+Value builtin_array_distinct(int argCount, Value *args)
+{
+    if (argCount != 1 || args[0].type != VAL_ARRAY)
+    {
+        print_error("distinct() requires an Array.");
+        return (Value){VAL_NIL, {0}};
+    }
 
+    ValueArray *old_arr = args[0].as.array;
+    ValueArray *new_arr = array_new();
+
+    for (int i = 0; i < old_arr->count; i++)
+    {
+        bool is_dup = false;
+        for (int j = 0; j < new_arr->count; j++)
+        {
+            Value eq = eval_equals(old_arr->values[i], new_arr->values[j]);
+            if (eq.as.number == 1.0)
+            {
+                is_dup = true;
+                break;
+            }
+        }
+        if (!is_dup)
+        {
+            array_append(new_arr, copy_value(old_arr->values[i]));
+        }
+    }
+    return (Value){VAL_ARRAY, {.array = new_arr}};
+}
+void register_array_natives(Env *env){
+    ARRAY_REGISTER(env,"__array_distinc",builtin_array_distinct);
 }
