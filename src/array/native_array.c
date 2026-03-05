cf6015d164aacefc0f2dcb75d22ffd1fa97d3361
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-03-05 10:06:46 +0700

    add native array templates

---
+++ src/array/native_array.c
@@ -0,0 +1,25 @@
+#include "array/native_array.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <float.h>
+#include "env.h"
+
+#define ARRAY_REGISTER(env, name, func)                                           \
+    do                                                                           \
+    {                                                                            \
+        if (func != NULL)                                                        \
+        {                                                                        \
+            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
+        }                                                                        \
+        else                                                                     \
+        {                                                                        \
+            printf("Error: Native function '%s' not implemented!\n", name);      \
+        }                                                                        \
+    } while (0)
+
+void register_array_natives(Env *env){
+
+}
