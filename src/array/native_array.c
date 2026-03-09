199646c6e073fd9ac74b0bab34b536744587e56f
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-03-09 20:53:45 +0700

    add array filter and map

--- src/array/native_array.c
+++ src/array/native_array.c
@@ -79,6 +79,52 @@ Value builtin_array_anyMatch(int argCount, Value *args)
     return (Value){VAL_NUMBER, {.number = 0.0}};
 }
 
+Value builtin_array_map(int argCount, Value *args)
+{
+    if (argCount != 2 || args[0].type != VAL_ARRAY || args[1].type != VAL_FUNCTION)
+    {
+        print_error("map() expects (Array, Callback).");
+        return (Value){VAL_NIL, {0}};
+    }
+
+    ValueArray *old_arr = args[0].as.array;
+    Value callback = args[1];
+    ValueArray *new_arr = array_new();
+
+    for (int i = 0; i < old_arr->count; i++)
+    {
+        Value arg = old_arr->values[i];
+        Value new_val = call_jackal_function(NULL, callback, 1, &arg);
+        array_append(new_arr, new_val);
+    }
+
+    return (Value){VAL_ARRAY, {.array = new_arr}};
+}
+Value builtin_array_filter(int argCount, Value *args)
+{
+    if (argCount != 2 || args[0].type != VAL_ARRAY || args[1].type != VAL_FUNCTION)
+    {
+        print_error("filter() expects (Array, Callback).");
+        return (Value){VAL_NIL, {0}};
+    }
+
+    ValueArray *old_arr = args[0].as.array;
+    Value callback = args[1];
+    ValueArray *new_arr = array_new();
+
+    for (int i = 0; i < old_arr->count; i++)
+    {
+        Value arg = old_arr->values[i];
+        Value result = call_jackal_function(NULL, callback, 1, &arg);
+        if (is_value_truthy(result))
+        {
+            array_append(new_arr, copy_value(arg));
+        }
+        free_value(result);
+    }
+
+    return (Value){VAL_ARRAY, {.array = new_arr}};
+}
 void register_array_natives(Env *env){
     ARRAY_REGISTER(env,"__array_distinct",builtin_array_distinct);
     ARRAY_REGISTER(env,"__array_anyMatch",builtin_array_anyMatch);
