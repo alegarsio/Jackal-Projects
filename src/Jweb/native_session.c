3d22a71b2555b12028813622987512eb15939956
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-03-23 07:44:58 +0700

    add new native template register

---
+++ src/Jweb/native_session.c
@@ -0,0 +1,29 @@
+#include "Jweb/native_session.h"
+#include "json/native_json.h"
+#include "eval.h"
+#include "value.h"
+
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <string.h>     
+#include <unistd.h>     
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <arpa/inet.h>   
+#include <fcntl.h>       
+#include <pthread.h>
+
+
+
+#define JWEB_SESSION_REGISTER(env, name, func)                                           \
+    do                                                                           \
+    {                                                                            \
+        if (func != NULL)                                                        \
+        {                                                                        \
+            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
+        }                                                                        \
+    } while (0)
+
+void register_session_native(Env* env){
+    
+}
