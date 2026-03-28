388aa3fd7805a2fd99af5a597252152f75bce9ce
Alegrarsio <121482257+alegarsio@users.noreply.github.com>
2026-03-28 08:57:01 +0700

    add protection in http native library

--- src/http/native_http.c
+++ src/http/native_http.c
@@ -3,23 +3,24 @@
 #include <string.h>
 #include <errno.h>
 #include <math.h>
-#include<sys/stat.h>
-#include<curl/curl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include "env.h"
+
+#if __has_include(<curl/curl.h>)
+    #include <curl/curl.h>
+    #define HAS_CURL 1
+#else
+    #define HAS_CURL 0
+#endif
 
 #define HTTP_REGISTER(env, name, func)                                           \
     do                                                                           \
     {                                                                            \
-        if (func != NULL)                                                        \
-        {                                                                        \
-            set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, ""); \
-        }                                                                        \
-        else                                                                     \
-        {                                                                        \
-            printf("Error: Native function '%s' not implemented!\n", name);      \
-        }                                                                        \
+        set_var(env, name, (Value){VAL_NATIVE, {.native = func}}, true, "");     \
     } while (0)
 
-
+#if HAS_CURL
 struct HttpBuffer {
     char *data;
     size_t size;
@@ -30,7 +31,7 @@ static size_t write_callback(void *contents, size_t size, size_t nmemb, void *us
     struct HttpBuffer *mem = (struct HttpBuffer *)userp;
 
     char *ptr = realloc(mem->data, mem->size + realsize + 1);
-    if (!ptr) return 0; // Out of memory
+    if (!ptr) return 0;
 
     mem->data = ptr;
     memcpy(&(mem->data[mem->size]), contents, realsize);
@@ -39,10 +40,12 @@ static size_t write_callback(void *contents, size_t size, size_t nmemb, void *us
 
     return realsize;
 }
+#endif
+
 Value native_http_get(int arity, Value *args) {
-    
+#if HAS_CURL
     if (arity < 1 || args[0].type != VAL_STRING) {
-        return (Value){VAL_NIL, {0}};
+        return (Value){VAL_NIL};
     }
 
     CURL *curl_handle;
@@ -56,7 +59,6 @@ Value native_http_get(int arity, Value *args) {
         curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
         curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-        
         curl_easy_setopt(curl_handle, CURLOPT_TIMEOUT, 10L);
 
         res = curl_easy_perform(curl_handle);
@@ -64,36 +66,36 @@ Value native_http_get(int arity, Value *args) {
         if (res != CURLE_OK) {
             free(chunk.data);
             curl_easy_cleanup(curl_handle);
-            return (Value){VAL_NIL, {0}};
+            return (Value){VAL_NIL};
         }
-
         curl_easy_cleanup(curl_handle);
     }
 
     Value result = (Value){VAL_STRING, {.string = strdup(chunk.data)}};
     free(chunk.data);
     return result;
+#else
+    fprintf(stderr, "Error: HTTP support (libcurl) not compiled into Jackal.\n");
+    return (Value){VAL_NIL};
+#endif
 }
 
 Value native_http_post(int arity, Value *args) {
+#if HAS_CURL
     if (arity < 2 || args[0].type != VAL_STRING || args[1].type != VAL_STRING) {
-        return (Value){VAL_NIL, {0}};
+        return (Value){VAL_NIL};
     }
 
     CURL *curl_handle;
     CURLcode res;
     struct HttpBuffer chunk = {malloc(1), 0};
 
-    curl_global_init(CURL_GLOBAL_ALL);
     curl_handle = curl_easy_init();
 
     if (curl_handle) {
         curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
-        
         curl_easy_setopt(curl_handle, CURLOPT_POST, 1L);
-        
         curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, args[1].as.string);
-
         curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
         curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Jackal-Interpreter/1.0");
@@ -103,19 +105,23 @@ Value native_http_post(int arity, Value *args) {
         if (res != CURLE_OK) {
             free(chunk.data);
             curl_easy_cleanup(curl_handle);
-            return (Value){VAL_NIL, {0}};
+            return (Value){VAL_NIL};
         }
-
         curl_easy_cleanup(curl_handle);
     }
 
     Value result = (Value){VAL_STRING, {.string = strdup(chunk.data)}};
     free(chunk.data);
     return result;
+#else
+    return (Value){VAL_NIL};
+#endif
 }
+
 Value native_http_get_headers(int arity, Value *args) {
+#if HAS_CURL
     if (arity < 2 || args[0].type != VAL_STRING || args[1].type != VAL_ARRAY) {
-        return (Value){VAL_NIL, {0}};
+        return (Value){VAL_NIL};
     }
 
     CURL *curl_handle;
@@ -136,30 +142,31 @@ Value native_http_get_headers(int arity, Value *args) {
 
         curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
         curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, header_list);
-        
         curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
         curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Jackal-Interpreter/1.0");
 
         res = curl_easy_perform(curl_handle);
-
         curl_slist_free_all(header_list);
         
         if (res != CURLE_OK) {
             free(chunk.data);
             curl_easy_cleanup(curl_handle);
-            return (Value){VAL_NIL, {0}};
+            return (Value){VAL_NIL};
         }
-
         curl_easy_cleanup(curl_handle);
     }
 
     Value result = (Value){VAL_STRING, {.string = strdup(chunk.data)}};
     free(chunk.data);
     return result;
+#else
+    return (Value){VAL_NIL};
+#endif
 }
 
 Value native_http_get_status(int arity, Value *args) {
+#if HAS_CURL
     if (arity < 1 || args[0].type != VAL_STRING) return (Value){VAL_NUMBER, {.number = 0}};
 
     CURL *curl_handle = curl_easy_init();
@@ -167,9 +174,7 @@ Value native_http_get_status(int arity, Value *args) {
 
     if (curl_handle) {
         curl_easy_setopt(curl_handle, CURLOPT_URL, args[0].as.string);
-        
         curl_easy_setopt(curl_handle, CURLOPT_NOBODY, 1L); 
-
         CURLcode res = curl_easy_perform(curl_handle);
 
         if (res == CURLE_OK) {
@@ -177,9 +182,12 @@ Value native_http_get_status(int arity, Value *args) {
         }
         curl_easy_cleanup(curl_handle);
     }
-
     return (Value){VAL_NUMBER, {.number = (double)response_code}};
+#else
+    return (Value){VAL_NUMBER, {.number = 0}};
+#endif
 }
+
 void register_http_natives(Env *env){
     HTTP_REGISTER(env,"__http_get",native_http_get);
     HTTP_REGISTER(env,"__http_post",native_http_post);
